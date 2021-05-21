23e5cc8bfbfdfca17914b0721c964312352d264d
ssonghyun101 <songhyun0868@gmail.com>
2021-05-21 09:30:55 +0900

    탭 뷰 위치 수정

--- Coinone/app/src/main/java/org/sopt/coinone/HomeFragmnet/PagerAdapter.kt
+++ Coinone/app/src/main/java/org/sopt/coinone/HomeFragmnet/PagerAdapter.kt
@@ -21,10 +21,10 @@ class PagerAdapter(fragmentManager: FragmentManager) : FragmentStatePagerAdapter
 
     override fun getItem(position: Int) : Fragment {
         val fragment= when(position){
-            1-> MyFragment()
-            2-> CoinListFragment()
-            3-> GumaeFragment()
-            else-> InfoFragment()
+            1-> CoinListFragment() //실제론 두번쨰
+            2->  GumaeFragment() //실제론 세번쨰
+            3-> InfoFragment()//실제론 네번재
+            else->  MyFragment() //실제론 첫번쨰
         }
         return fragment
 
@@ -32,10 +32,10 @@ class PagerAdapter(fragmentManager: FragmentManager) : FragmentStatePagerAdapter
 
     override fun getPageTitle(position: Int): CharSequence? {
        val title = when(position){
-           1->"마이"
-           2->"거래소"
-           3->"간편구매"
-           else->"정보"
+           1->"거래소" //실제론 두번쨰
+           2->"간편구매" //실제론 세번째
+           3->"정보" //실제론 네번쨰
+           else->"마이" //실제론 첫번쨰
        }
         return title
     }
