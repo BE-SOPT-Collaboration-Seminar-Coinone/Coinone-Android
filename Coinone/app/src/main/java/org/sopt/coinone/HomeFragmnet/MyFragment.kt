f6be0f68c4ec8ac13bcf92da9542608b7caf57b3
teagh82 <teagh8237@gmail.com>
2021-05-24 11:06:59 +0900

    My RecyclerView 수정

--- Coinone/app/src/main/java/org/sopt/coinone/HomeFragmnet/MyFragment.kt
+++ Coinone/app/src/main/java/org/sopt/coinone/HomeFragmnet/MyFragment.kt
@@ -58,6 +58,15 @@ class MyFragment:Fragment() {
                     percent = "(+3.64%)",
                     state="+"
                 ),
+                MyFavoriteListInfo(
+                    logo="https://lh3.googleusercontent.com/proxy/OnCK_oJvRkdQl_RbFgSJfyQX7cGrONtYPMMMduPHm-naBBdEE9cDs_i-WNx0CRK5J3DkmdJEhIVMkjoQ7qrhI1PRb_mCO6cg3Luihc7vARnTc2jEyPMHfmD4ZsJsRGgIeh6RmKi3gesdpkcj2oEeYvWW5cDDgrrD4sfe1gv313HND_LC-FAWBmeJVWSekiq3GUF9618Q_yqkr20WP0d8gj35M-Bp-NUYyKQpSuwdB4xMXqbQV6i-PC8bWCk",
+                    name_k = "클레이튼",
+                    name_e = "KLAY",
+                    price = "1,900.0",
+                    degree = "66.8",
+                    percent = "(+3.64%)",
+                    state="+"
+                ),
             )
         )
         // 데이터 변경되었으니 업데이트해라
